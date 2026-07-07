0278fb388e4d3fffb1cdc5cd11227265ead22256
msafei <msafei.ti@gmail.com>
2026-07-07 23:48:11 +0700

    favicon fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Admin Panel') }}</title>
+    <title>Admin | Svei Portofolio</title>
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('assets/img/logo.svg') }}" type="image/svg+xml">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
